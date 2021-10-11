c7d3e28fa3e220802283a0d0f2745fa5aa5b02fa
Christoph Hu <christoph.huschenhoefer@gmx.de>
2021-10-11 07:21:55 +0200

    ngrx effects

---
+++ ngrx/src/app/auth/state/effects.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Actions, ofType, createEffect } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { catchError, exhaustMap, map } from 'rxjs/operators';
+ 
+@Injectable()
+export class AuthEffects {
+    login$ = createEffect(() =>
+        this.actions$.pipe(
+        ofType(LoginPageActions.login),
+        exhaustMap(action =>
+            this.authService.login(action.credentials).pipe(
+            map(user => AuthApiActions.loginSuccess({ user })),
+            catchError(error => of(AuthApiActions.loginFailure({ error })))
+            )
+        )
+        )
+    );
+ 
+    constructor(
+        
+    ) {}
+}
