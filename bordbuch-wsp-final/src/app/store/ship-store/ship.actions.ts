eabab3d8d916cd2c232eaed65eab330b03cf05e2
Christoph Hu <christoph.huschenhoefer@gmx.de>
2021-10-18 15:37:03 +0200

    simple shipState funct

--- bordbuch-wsp-final/src/app/store/ship-store/ship.actions.ts
+++ bordbuch-wsp-final/src/app/store/ship-store/ship.actions.ts
@@ -3,11 +3,17 @@ import { Ship } from "src/app/core/model/ship.model"
 
 // load data
 export const loadShip = createAction(
-	"[Ship Resolver] Load All Data"
+	"[Ship Resolver] Load Ship",
+	props<{ id_ship: string }>()
 )
 
 // store loaded data
 export const shipLoaded = createAction(
 	"[Load Ship Effect] Ship Loaded",
-	props<{ship: Ship[]}>()
+	props<{ ship: Ship }>()
+)
+
+export const loadPatrol = createAction(
+	"[Ship Resolver] Load Patrol",
+	props<{ id_ship: string }>()
 )
