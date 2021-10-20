6df35d1ce29318e9c045c28dc714d5749256f952
Christoph Hu <christoph.huschenhoefer@gmx.de>
2021-10-20 16:52:02 +0200

    wark on routing to /map

--- bordbuch-wsp-final/src/app/modules/components/boot/boot.module.ts
+++ bordbuch-wsp-final/src/app/modules/components/boot/boot.module.ts
@@ -3,7 +3,6 @@ import { BootComponent } from './boot.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ShipResolver } from 'src/app/store/ship-store/ship.resolver';
 import { ShipModule } from 'src/app/store/ship-store/ship.module';
-// import { _actionTypeUniquenessCheck } from '@ngrx/store/src/runtime_checks';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { KatModule } from 'src/app/store/kat-store/kat.module';
 import { BesatzungComponent } from './besatzung/besatzung.component';
@@ -12,12 +11,13 @@ import { PruefvermerkComponent } from './pruefvermerk/pruefvermerk.component';
 import { BetankungComponent } from './betankung/betankung.component';
 import { DataModule } from 'src/app/store/positionreport-store/data.module';
 import { ZaehlerstandModule } from 'src/app/store/zaehlerstand-store/data.module';
+import { MapComponent } from '../map/map.component';
 
 export const routes: Routes = [
   { path: '', component: BootComponent,
     children: [
       { path: ':id', component: BootComponent, resolve: { data: ShipResolver }, data: { param: 'id'} },
-      // { path: ':id/map', component: MapComponent },
+      { path: ':id/map', component: MapComponent },
       // { path: 'boot/:id/positions', component: PositionenComponent },
 
       // { path: '**', redirectTo: '' }
@@ -31,7 +31,8 @@ export const routes: Routes = [
     BesatzungComponent,
     ZaehlerstandComponent,
     PruefvermerkComponent,
-    BetankungComponent
+    BetankungComponent,
+    MapComponent
   ],
   imports: [
     SharedModule,
